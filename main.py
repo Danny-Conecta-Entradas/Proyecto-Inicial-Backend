0b90f1cbb89ecc187bd0ab2ae24b56c8865ed0fa
Daniel Fabián <daniel@conectaentradas.com>
2024-03-14 13:09:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ app.add_middleware(
   CORSMiddleware,
   allow_origins=['*'],
   # allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+  # allow_credentials=True,
+  # allow_methods=["*"],
+  # allow_headers=["*"],
 )
 
 
