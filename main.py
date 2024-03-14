4f0601f70c2508cfa16d2fe0b52ee75457573ee9
Daniel Fabián <daniel@conectaentradas.com>
2024-03-14 11:47:10 +0100

    Format code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from google.cloud import datastore
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from utils import cors_configuration, create_and_store_entity, FormData
+
 
 app = FastAPI()
 
@@ -17,13 +17,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-database_name = 'registro'
-datastore_client = datastore.Client(database = database_name)
 
-
-class FormData(BaseModel):
-  timestamp: str
-  string_qr: str
+bucket_name = 'artifacts.proyecto-inicial-daniel.appspot.com'
+cors_configuration(bucket_name)
 
 
 @app.post('/api/send-data/')
@@ -31,18 +27,3 @@ def send_data(item: FormData):
   create_and_store_entity(item)
 
   return item
-
-
-def create_and_store_entity(form_data: FormData):
-  kind = FormData.__name__
-  form_key = datastore_client.key(kind)
-
-  form_entity = datastore.Entity(key = form_key)
-  form_entity.update({
-    'timestamp': form_data.timestamp,
-    'string_qr': form_data.string_qr,
-  })
-
-  datastore_client.put(form_entity)
-
-  return form_data
---
+++ utils.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+from google.cloud import storage
+from google.cloud import datastore
+
+class FormData(BaseModel):
+  timestamp: str
+  string_qr: str
+
+
+database_name = 'registro'
+datastore_client = datastore.Client(database = database_name)
+
+def create_and_store_entity(form_data: FormData):
+  kind = FormData.__name__
+  form_key = datastore_client.key(kind)
+
+  form_entity = datastore.Entity(key = form_key)
+  form_entity.update({
+    'timestamp': form_data.timestamp,
+    'string_qr': form_data.string_qr,
+  })
+
+  datastore_client.put(form_entity)
+
+  return form_data
+
+
+def cors_configuration(bucket_name):
+  """Set a bucket's CORS policies configuration."""
+
+  storage_client = storage.Client()
+
+  bucket = storage_client.get_bucket(bucket_name)
+  bucket.cors = [
+    {
+      'origin': ['*'],
+      'responseHeader': [
+        'Content-Type',
+        'x-goog-resumable',
+      ],
+      'method': ['PUT', 'POST'],
+      'maxAgeSeconds': 3600,
+    },
+  ]
+
+  bucket.patch()
