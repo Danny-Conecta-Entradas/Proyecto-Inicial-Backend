82431e6f7d75d8181a919c1f6401312a532acc76
Daniel Fabián <daniel@conectaentradas.com>
2024-03-14 12:35:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ origins = [
 ]
 
 middleware = [
-  Middleware(CORSMiddleware, allow_origins = origins)
+  Middleware(CORSMiddleware, allow_origins = origins, allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 ]
 
 app = FastAPI(middleware = middleware)
