178c004688b2bf4073f0a1cd0ba590f7d5c9b657
Daniel Fabián <daniel@conectaentradas.com>
2024-03-14 12:30:46 +0100

    Try to fix CORS

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from utils import cors_configuration, create_and_store_entity, FormData
 
-
-app = FastAPI()
 
 origins = [
   'https://proyecto-inicial-backend-agk6kyxhfa-uc.a.run.app',
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['https://youtube.com'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+middleware = [
+  Middleware(CORSMiddleware, allow_origins = origins)
+]
+
+app = FastAPI(middleware = middleware)
+
+# origins = [
+#   'https://proyecto-inicial-backend-agk6kyxhfa-uc.a.run.app',
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 bucket_name = 'artifacts.proyecto-inicial-daniel.appspot.com'
