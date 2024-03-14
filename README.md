b3bb478d5b4f05735ae1e7db9dc0626aa18ffc7d
Daniel Fabián <daniel@conectaentradas.com>
2024-03-14 10:48:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Proyecto-Inicial
+
+
+```bash
+# Add to git case sensitive rename
+git mv MyFile.txt myfile.txt
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from google.cloud import datastore
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+database_name = 'registro'
+datastore_client = datastore.Client(database = database_name)
+
+
+class FormData(BaseModel):
+  timestamp: str
+  string_qr: str
+
+
+@app.post('/api/send-data')
+def send_data(item: FormData):
+  create_and_store_entity(item)
+
+  return item
+
+
+def create_and_store_entity(form_data: FormData):
+  kind = FormData.__name__
+  form_key = datastore_client.key(kind)
+
+  form_entity = datastore.Entity(key = form_key)
+  form_entity.update({
+    'timestamp': form_data.timestamp,
+    'string_qr': form_data.string_qr,
+  })
+
+  datastore_client.put(form_entity)
+
+  return form_data
